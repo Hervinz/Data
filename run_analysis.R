a2338fba741cc9b8951d0be4291968a28d560d1a
Hervinz <hszuluagas@unal.edu.co>
2020-05-22 13:33:24 -0500

    "features.txt" file missed is loaded

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,3 @@
-if(!file.exists("./datab")){dir.create("./datab")}
-
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 download.file(url = fileUrl, destfile = "UCIHAR.zip")
 
@@ -30,14 +28,23 @@ x_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
 y_test <- read.table("./UCI HAR Dataset/test/y_test.txt") 
 
 
-## CÓDGICO Y NOMBRE DE CADA UNA DE LAS 6 ACTIVIDADES OBSERVADAS
+
+####NOMBRE DE CADA UNA DE LAS 561 SEÑALES DE LAS QUE SE TOMARON DATOS,
+#INCLUYE NUMERACIÓN Y NOMBRE DE LA CARACTERÍSTICA
+features <- read.table("./UCI HAR Dataset/features.txt") 
+
+
+
+####CÓDGICO Y NOMBRE DE CADA UNA DE LAS 6 ACTIVIDADES OBSERVADAS
 activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")
 
+
+
 #Cantidad filas y columnas por archivo
-observaciones <- data.frame("subject_train" = dim(subject_train),
+observaciones <- data.frame("features" = dim(features),
+                            "actividades" = dim(activity_labels), row.names = c("NRows", "NCols"),
+                            "subject_train" = dim(subject_train),
                             "x_train" = dim(x_train), "y_train" = dim(y_train),
                             "subject_test" = dim(subject_test),
-                            "x_test" = dim(x_test), "y_test" = dim(y_test),
-                            "actividades" = dim(activity_labels), row.names = c("NRows", "NCols"))
-
+                            "x_test" = dim(x_test), "y_test" = dim(y_test))
 observaciones
